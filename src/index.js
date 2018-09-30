0808e1998b36bfa6d6b4e225d5c3824f7efdeb95
taukitianin <newvivan@gmail.com>
2018-09-30 22:55:43 +0300

    ok

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  
+ // number = 1000;
+  let zero = 0;
+  while(number>0){
+    number = Math.floor(number/5);
+    zero += number;
+
+  }
+  return zero;
 }
